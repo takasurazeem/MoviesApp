3743d8949f4c42d32859a6669c9554feb5a14c4d
Takasur Azeem <takasurazeem@gmail.com>
2024-05-30 11:24:47 +0500

    Fix broken project....

---
+++ MoviesApp/Utils/ApiKeys.swift
@@ -0,0 +1,19 @@
+//
+//  ApiKeys.swift
+//  MoviesApp
+//
+//  Created by Takasur Home on 30/05/2024.
+//  Copyright © 2024 JABERI Mohamed. All rights reserved.
+//
+
+import Foundation
+
+extension Constants {
+    struct ApiKeys {
+        
+        /// Obtain your OMDB Key from here: https://www.omdbapi.com/apikey.aspx
+        static var omdbIdKey: String {
+            fatalError("Return your API key here.")
+        }
+    }
+}
