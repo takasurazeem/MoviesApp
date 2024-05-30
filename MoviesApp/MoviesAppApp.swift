d91fc643836534643fb2b738e034c0c5c2c811ff
Takasur Azeem <takasurazeem@gmail.com>
2024-05-30 11:18:57 +0500

    Fix broken project....

--- MoviesApp/MoviesAppApp.swift
+++ MoviesApp/MoviesAppApp.swift
@@ -1,9 +1,14 @@
-
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Takasur Home on 30/05/2024.
+//
 
 import SwiftUI
 
 @main
-struct HelloReduxApp: App {
+struct MoviesAppApp: App {
     var body: some Scene {
        
         let store = Store(reducer: appReducer, state: AppState(), middlewares: [moviesMiddleware()])
