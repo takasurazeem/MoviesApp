632a0565b18d2543722064ea8a3e6e6585742e7c
Takasur Azeem <takasurazeem@gmail.com>
2024-05-30 11:47:39 +0500

    Create a Webservice.

--- MoviesApp/Services/Webservice.swift
+++ MoviesApp/Services/Webservice.swift
@@ -9,4 +9,22 @@ enum NetworkError: Error {
 
 class Webservice {
     
+    func getMoviesBy(search: String) async throws -> [Movie] {
+        guard let moviesUrl = URL(string: Constants.Urls.urlBySearch(search: search)) else { throw NetworkError.badURL }
+        return try await withCheckedThrowingContinuation { continuation in
+            URLSession.shared.dataTask(with: moviesUrl) { data, response, error in
+                guard let data, error == nil else {
+                    continuation.resume(throwing: NetworkError.noData)
+                    return
+                }
+                do {
+                    let moviesResponse = try JSONDecoder().decode(MovieResponse.self, from: data)
+                    continuation.resume(returning: moviesResponse.movies)
+                } catch {
+                    continuation.resume(throwing: NetworkError.decodingError)
+                }
+            }.resume()
+        }
+    }
+    
 }
